27ac07a6677d17ecc9c60d36f4b900e2d0f63aa4
Babou Entreprise <96743733+BabouEntreprise@users.noreply.github.com>
2022-09-12 21:34:26 +0200

    Update cloudMiner1trex.sh

--- cloudMiner1trex.sh
+++ cloudMiner1trex.sh
@@ -1,3 +1,11 @@
+sudo apt update
+clear
+sudo apt install screen
+screen -R trex
 
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.5/t-rex-0.26.5-linux.tar.gz
 
--a kawpow -o kp.unmineable.com:3333 -u TRX:TNKxB3LmEcsgZUuPHvzA56E3DdxpQXTZmN.cloudminertrex -p x
+tar xvzf t-rex-0.26.5-linux.tar.gz
+cd t-rex-0.26.5
+
+./t-rex -a kawpow -o kp.unmineable.com:3333 -u TRX:TNKxB3LmEcsgZUuPHvzA56E3DdxpQXTZmN.cloudminertrex -p x
